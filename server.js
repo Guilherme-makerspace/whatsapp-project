f59d08d951b33d9891a33f5958c38f2feb191d83
Guilherme-makerspace <guilhermeoliveiraaraujo1603@gmail.com>
2026-03-17 16:57:56 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Usuário conectado:', socket.id);
+
+    socket.on('send_message', (data) => {   
+        io.emit('receive_message', data);
+    });
+
+    socket.on('disconnect', () => console.log('Usuário saiu'));
+});
+
+server.listen(3000, () => console.log('Servidor rodando na porta 3000'));
